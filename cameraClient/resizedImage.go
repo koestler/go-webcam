18b0af8df93677a9ea1f9d0c85db0f4864888250
Lorenz Koestler <lorenz@koestler.ch>
2021-08-08 13:49:45 +0200

    refactoring

--- cameraClient/resizedImage.go
+++ cameraClient/resizedImage.go
@@ -1,6 +1,10 @@
 package cameraClient
 
 import (
+	"bufio"
+	"bytes"
+	"github.com/disintegration/imaging"
+	"image/jpeg"
 	"log"
 	"strconv"
 )
@@ -47,4 +51,52 @@ func (c *Client) handleResizedImageReadRequest(request resizedImageReadRequest) 
 		request.response <- resizedImage
 		c.resizeCache[cacheKey] = resizedImage
 	}
+}
+
+func imageResize(inpImg []byte, requestedWidth, requestedHeight int) (oupImg []byte, err error) {
+	if len(inpImg) == 0 {
+		return inpImg, nil
+	}
+
+	decodedImg, err := jpeg.Decode(bytes.NewReader(inpImg))
+	if err != nil {
+		return
+	}
+
+	bounds := decodedImg.Bounds()
+	inputWidth := bounds.Max.X - bounds.Min.X
+	inputHeight := bounds.Max.Y - bounds.Min.Y
+
+	var width, height int
+
+	if requestedWidth*inputHeight/inputWidth < requestedHeight {
+		width = min(inputWidth, requestedWidth)
+		height = 0
+	} else {
+		width = 0
+		height = min(inputHeight, requestedHeight)
+	}
+
+	if inputWidth == width || inputHeight == height {
+		return inpImg, nil
+	}
+
+	resizedImp := imaging.Resize(decodedImg, width, height, imaging.Box)
+
+	var b bytes.Buffer
+	w := bufio.NewWriter(&b)
+
+	err = jpeg.Encode(w, resizedImp, &jpeg.Options{Quality: 90})
+	if err != nil {
+		return
+	}
+
+	return b.Bytes(), nil
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
 }
